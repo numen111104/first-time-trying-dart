55b76ebff04770ff199682c15336c98abb243776
numen111104 <numannasyarmz11@gmail.com>
2024-01-12 22:57:47 +0700

    update function

---
+++ bin/Jumat-12-01/ListData.dart
@@ -0,0 +1,45 @@
+Future<void> main() async {
+  final List<String> arrayDart = ["Angka1"];
+
+  arrayDart.add("Makanan");
+  print(arrayDart);
+  String a = arrayDart[0];
+  print(a.substring(1, 2));
+
+  addingArrayList(20);
+
+
+}
+
+// TODO: Buatlah sebuah fungsi yang memasukkan angka ganjil 1-20 ke dalam empty list
+
+dynamic addingArrayList(int angka) {
+  var ganjilArray = <int>[];
+  var genapArray = <int>[];
+  for (var i = 0; i < angka; i++) {
+    if (i % 2 != 0) {
+      print("ganjil: $i");
+      ganjilArray.add(i);
+    } else {
+      print("genap: $i");
+      genapArray.add(i);
+    }
+  }
+  print("List array ganjil = $ganjilArray\n"
+      "List array genap = $genapArray");
+   delTwoIndex(ganjilArray, genapArray);
+}
+
+void delTwoIndex(List<int> arrayGanjil, List<int> arrayGenap) {
+  var finalGanjil = <int>[];
+  var finalGenap = <int>[];
+ for(var i = 0; i < arrayGanjil.length; i++) {
+   finalGanjil.add(arrayGanjil.removeAt(i));
+ }
+ for(var i = 0; i < arrayGenap.length; i++) {
+   finalGenap.add(arrayGenap.removeAt(i));
+ }
+
+  print('List setelah dihapus 2 index genap: $finalGenap');
+ print('List setelah dihapus 2 index ganjil: $finalGanjil');
+}
